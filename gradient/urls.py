837d882f5ab07f2a9847d0212698cdc2d9312125
Sergey Kovalchuk <info@sergart.ru>
2018-09-04 04:44:23 +0300

    wtf

--- gradient/urls.py
+++ gradient/urls.py
@@ -1,5 +1,5 @@
 """
-gradient URL Configuration
+Gradient URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+
 from django.conf.urls.static import static
 from django.conf import settings
 
